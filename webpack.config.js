8340c685b4707f258b5d3c5800137eb079b9de27
jose <joseih2010@hotmail.com>
2024-04-09 20:51:10 -0300

    First

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(jpg|png|svg|gif)$/,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      tittle: "Restaurant",
+      filename: "index.html",
+      inject: "head",
+      scriptLoading: "defer",
+    }),
+  ],
+};
